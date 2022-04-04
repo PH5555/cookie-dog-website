68e7680b841f9cffaa6c539562b77d853067b4bf
김동현 <47862506+PH5555@users.noreply.github.com>
2022-04-04 14:08:27 +0900

    add image, font

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ function App() {
   return (
     <div className="App">
       <div className='App-Bar'>
-        <div className='Logo'>
-          
-        </div>
+        
+        <img className="Logo"  src="img/logo.png" />
       
         <div className='Menu'>
-          <p className='Menu-List' id='Home'>HOME</p>
-          <p className='Menu-List'>Prologue</p>
-          <p className='Menu-List'>ALL COOKIES</p>
-          <p className='Menu-List'>CookieDog Team</p>
-          <p className='Menu-List'>Opensea</p>
+          <p id='Home'>HOME</p>
+          <p>Prologue</p>
+          <p>ALL COOKIES</p>
+          <p>CookieDog Team</p>
+          <p>Opensea</p>
           <div className='Connect-Wallet-Button'>
             connect wallet
           </div>
         </div>
       </div>
       <div className='Main-Box'>
-        <p className='Title'>GET YOUR OWN MUSIC in COOKIES</p>
+        <div className='Title-Box'>
+          <p className='Title'>GET YOUR OWN MUSIC in COOKIES</p>
+          <img className="Main-Image"  src="img/planet.png" />
+        </div>
+       
         <div className='Mint-Box'>
         ff
         </div>
