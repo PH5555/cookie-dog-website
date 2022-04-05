85244864b3fd01806dc7de2fa6c428072aa9308d
김동현 <47862506+PH5555@users.noreply.github.com>
2022-04-05 09:52:47 +0900

    메인 박스 작업중

--- src/App.js
+++ src/App.js
@@ -6,31 +6,46 @@ function App() {
 
   return (
     <div className="App">
-      <div className='App-Bar'>
-        
-        <img className="Logo"  src="img/logo.png" />
-      
-        <div className='Menu'>
-          <p id='Home'>HOME</p>
-          <p>Prologue</p>
-          <p>ALL COOKIES</p>
-          <p>CookieDog Team</p>
-          <p>Opensea</p>
-          <div className='Connect-Wallet-Button'>
-            connect wallet
+
+      <div className='Top-Area'>
+        <div className='App-Bar'>
+          <img className="Logo"  src="img/logo.png" />
+          <div className='Menu'>
+            <p id='Home'>HOME</p>
+            <p>Prologue</p>
+            <p>ALL COOKIES</p>
+            <p>CookieDog Team</p>
+            <p>Opensea</p>
+            <div className='Connect-Wallet-Button'>connect wallet</div>
           </div>
         </div>
-      </div>
-      <div className='Main-Box'>
-        <div className='Title-Box'>
-          <p className='Title'>GET YOUR OWN MUSIC in COOKIES</p>
-          <img className="Main-Image"  src="img/planet.png" />
+        <div className='Main-Box'>
+          <div className='Title-Box'>
+            <p className='Main-Title'>GET YOUR OWN MUSIC in COOKIES</p>
+            <img className="Main-Image"  src="img/planet.png" />
+          </div>
+          <div className='Mint-Box'>
+            <div className='NFT-Image'></div>
+            <div className='NFT-Content'>
+              <p className='NTF-Name'>MuteHoon</p>
+              <p className='NFT-Number'>#078</p>
+            </div>
+            <div className='Date-And-Button'>
+              <p className='Date'>6 April, 12PM EDT</p>
+              <div className='Mint-Button'>Mint Now</div>
+              <p className='Current-Situation'>20/20</p>
+            </div>
+          </div>
+          
         </div>
-       
-        <div className='Mint-Box'>
-        ff
-        </div>
       </div>
+{/* 
+      <div className='View-Cookies-Area'>
+        <div className='Cookies'>
+          <div className='Cookies-Title'>ALL COOKIES</div>
+
+        </div>
+      </div> */}
     </div>
   );
 }
